a3f0d076931dcb5b7d18bbcbe97f14a09f3900a4
linarahmatin <linarahmatin9@gmail.com>
2023-09-27 08:15:40 +0700

    percobaan 1

--- PemilihanPercobaan128.java
+++ PemilihanPercobaan128.java
@@ -2,11 +2,12 @@ import java.util.Scanner;
 public class PemilihanPercobaan128 {
     public static void main(String[] args) {
            Scanner input28 = new Scanner(System.in);
-        System.out.print ("Masukan angka: ");
-        int angka = input28.nextInt();
-        if (angka % 2 == 0)
-            System.out.println("Angka "+angka+" bilangan genap");
-        else
-        System.out.println("Angka "+angka+" bilangan ganjil");
+           String hasil;
+
+           System.out.print ("Masukan angka: ");
+           int angka = input28.nextInt();
+    
+           hasil = (angka % 2 == 0)? "Bilangan Genap" : "Bilangan negatif";
+            System.out.println(angka + " adalah " + hasil);  
     }
 }
