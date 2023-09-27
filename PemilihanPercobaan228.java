3f26837cc4114015c15af5462144545d600a17a0
linarahmatin <linarahmatin9@gmail.com>
2023-09-27 09:17:59 +0700

    percobaan 2

--- PemilihanPercobaan228.java
+++ PemilihanPercobaan228.java
@@ -2,17 +2,25 @@ import java.util.Scanner;
 public class PemilihanPercobaan228 {
     public static void main(String[] args) {
         Scanner input28 = new Scanner(System.in);
-        System.out.print("Nilai uas  :");
-        float uas = input28.nextFloat(); 
-        System.out.print("Nilai uts : ");
-        float uts = input28.nextFloat(); 
-        System.out.print("Nilai kuis : ");
-        float kuis = input28.nextFloat();
-        System.out.print("Nilai tugas : ");
-        float tugas = input28.nextFloat();
-        float total = (uas * 0.4F) + (uts * 0.3F) + (kuis * 0.1F) + (tugas * 0.2F);
-        String message = total < 65 ? "Remidi" : "Tidak remidi";
-        System.out.println("Nilai akhir = " + total + " sehingga " +message);
+        double nilai;
+
+        System.out.println("Masukkan nilai :");
+        nilai = input28.nextDouble();
         
-    }
+        if (nilai > 80) {
+         System.out.println("hasil nilainya adalah " +nilai+ " A");
+        }else if (nilai > 73){
+         System.out.println("hasil nilainya adalah " +nilai+ " B+");
+        }else if (nilai > 65){
+         System.out.println("hasil nilainya adalah " +nilai+ " B");
+        }else if (nilai > 60){
+         System.out.println("hasil nilainya adalah " +nilai+ " C+"); 
+        }else if (nilai > 50){ 
+         System.out.println("hasil nilainya adalah " +nilai+ " C");
+        }else if (nilai > 39){
+         System.out.println("hasil nilainya adalah " +nilai+ " D");
+        }else{
+         System.out.println("hasil nilainya adalah " +nilai+ " E");
+        }
+     }
 }
