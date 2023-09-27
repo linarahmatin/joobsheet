f95b1902310e4886c0dcdf351f8514f15aecaecc
linarahmatin <linarahmatin9@gmail.com>
2023-09-27 09:47:38 +0700

    percobaan 3

--- PemilihanPercobaan328.java
+++ PemilihanPercobaan328.java
@@ -14,7 +14,6 @@ public class PemilihanPercobaan328 {
              case '+':
               hasil = angka1 + angka2; 
               System.out.println(angka1 +  " + " + angka2 + "=" + hasil);
-              break; 
              case '-':
               hasil = angka1 - angka2;
               System.out.println(angka1 + " - " + angka2 + "=" + hasil);
