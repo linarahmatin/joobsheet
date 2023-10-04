07962757d126c11a05d907bd02e8075b70edc0ce
linarahmatin <linarahmatin9@gmail.com>
2023-10-04 09:31:18 +0700

    modifikasi jenis bangunan datar segitiga

--- jobsheet6/Pemilihan2Percobaan227.java
+++ jobsheet6/Pemilihan2Percobaan227.java
@@ -3,26 +3,24 @@ public class Pemilihan2Percobaan227 {
     public static void main(String[] args) {
         Scanner input27 = new Scanner(System.in);
 
-        int sudut1, sudut2, sudut3, totalsudut;
-
         System.out.println("Masukkan sudut 1:");
-        sudut1 = input27.nextInt();
+        float sudut1 = input27.nextFloat();
         System.out.println("Masukkan sudut 2:");
-        sudut2 = input27.nextInt();
+        float sudut2 = input27.nextFloat();
         System.out.println("Masukkan sudut 3:");
-        sudut3 = input27.nextInt();
-
-        totalsudut = sudut1 + sudut2 + sudut3;
+        float sudut3 = input27.nextFloat();
+        float totalsudut = sudut1 + sudut2 + sudut3;
 
         if (totalsudut == 180) {
             if ((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90)) {
                 System.out.println("Segitiga tersebut adalah segitiga siku-siku");
+            } else if (sudut1 == sudut2 || sudut2 == sudut3 || sudut3 == sudut1) {
+                System.out.println("Segitiga tersebut adalah segitiga sama kaki");
+            }else{
+                System.out.println("Segitiga tersebut adalah segitiga sembarang");
             }
-            else{
-                System.out.println( "Segitiga tersebut adalah bukan segitiga siku-siku");
-            }
-        } else{
-            System.out.println( "Bukan Segitiga");
+            } else
+            System.out.println("Bukan Segitiga");
         }
     } 
-}
+
