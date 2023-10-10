d264d60449132762a2f6dd296cc45913e168fccb
linarahmatin <linarahmatin9@gmail.com>
2023-10-10 13:32:46 +0700

    hasil soal 1 nomer 3

--- jobsheet7/forKelipatan27.java
+++ jobsheet7/forKelipatan27.java
@@ -1,21 +1,25 @@
+
 import java.util.Scanner;
 
 public class forKelipatan27 {
     public static void main(String[] args) {
         Scanner input27 = new Scanner(System.in);
         int kelipatan, jumlah = 0, counter = 0;
-
+        double rataRata = (double)jumlah / counter;
+    
         System.out.print("Masukkan bilangan kelipatan (1-9): ");
-        kelipatan = input27 .nextInt();
+        kelipatan = input27.nextInt();
 
         for (int i = 1; i <= 50; i++) {
             if (i % kelipatan == 0) {
                 jumlah += i;
                 counter++;
+        
             }
         }
 
         System.out.println("Banyaknya bilangan " + kelipatan + " dari 1 sampai 50 adalah " + counter);
         System.out.println("Total bilangan kelipatan " + kelipatan + " dari 1 sampai 50 adalah " + jumlah);
+        System.out.println("Rata-rata bilangan kelipatan " + kelipatan + " dari 1 sampai 50 adalah " + rataRata);
     }
 }
