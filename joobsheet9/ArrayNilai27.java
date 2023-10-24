1168f328c757c33591df723c9966ec1ffa1a5c95
linarahmatin <linarahmatin9@gmail.com>
2023-10-24 19:21:31 +0700

    hasil dari percobaan ke 2

--- joobsheet9/ArrayNilai27.java
+++ joobsheet9/ArrayNilai27.java
@@ -2,17 +2,19 @@ package joobsheet9;
 import java.util.Scanner;
 public class ArrayNilai27 {
     public static void main(String[] args) {
-       Scanner input27 = new Scanner (System.in); 
+       Scanner sc = new Scanner (System.in); 
        int[] nilaiAkhir = new int[10];
 
-       for (int i = 0; i < 10; i++) {
-            System.out.print("Masukkan nilai akhir ke- " + i + " "); 
-            nilaiAkhir[i] = input27.nextInt();
+       for (int i = 0; i < nilaiAkhir.length; i++) {
+            System.out.print ("Masukkan nilai akhir ke-"+i+" : "); 
+            nilaiAkhir [i] = sc. nextInt ();
        }
 
-       for (int i = 0; i < 10; i++) { 
-            System.out.println("Nilai akhir ke-" + i + " adalah " +
-nilaiAkhir[i]);
-       }
-    }
-       }
+       for (int i = 0; i < nilaiAkhir.length; i++) {
+            if (nilaiAkhir [i] > 70) { 
+               System.out.println("Mahasiswa ke-"+i+" lulus!");
+               System.out.println("Mahasiswa ke-"+i+" tidak lulus!");
+            }
+         }
+      }
+}
