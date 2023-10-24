9343373644a2ebeaa38d3a01fd45ba9cd5ab6893
linarahmatin <linarahmatin9@gmail.com>
2023-10-24 10:16:51 +0700

    Hasil percobaan 1

---
+++ joobsheet9/ArrayNilai27.java
@@ -0,0 +1,18 @@
+package joobsheet9;
+import java.util.Scanner;
+public class ArrayNilai27 {
+    public static void main(String[] args) {
+       Scanner input27 = new Scanner (System.in); 
+       int[] nilaiAkhir = new int[10];
+
+       for (int i = 0; i < 10; i++) {
+            System.out.print("Masukkan nilai akhir ke- " + i + " "); 
+            nilaiAkhir[i] = input27.nextInt();
+       }
+
+       for (int i = 0; i < 10; i++) { 
+            System.out.println("Nilai akhir ke-" + i + " adalah " +
+nilaiAkhir[i]);
+       }
+    }
+       }
