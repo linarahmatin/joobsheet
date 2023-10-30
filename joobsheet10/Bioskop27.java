c1a8d07cd6f5440493bc628d6c4bbbc1106adec0
linarahmatin <linarahmatin9@gmail.com>
2023-10-30 20:42:16 +0700

    Hasil kode program pertanyaan ke 5

--- joobsheet10/Bioskop27.java
+++ joobsheet10/Bioskop27.java
@@ -10,9 +10,27 @@ public class Bioskop27 {
         penonton [2][0] = "Eka";
         penonton [2][1] = "Farhan";
         penonton [3][0] = "Gisel";
+        penonton [3][1] = "Hana";
+
         System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
         System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
         System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
         System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
+
+        System.out.println(penonton.length);
+        System.out.println(penonton[0].length);
+        System.out.println(penonton[1].length);
+        System.out.println(penonton[2].length);
+        System.out.println(penonton[3].length);
+
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Panjang baris ke-" + (i+1) + ": " + penonton[i].length);
+        }
+
+        for (String[] barisPenonton : penonton) {
+            System.out.println("Panjang baris: " + barisPenonton.length);
+        }
     } 
 }
+    
+
