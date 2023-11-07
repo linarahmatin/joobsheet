00f22b3e4cf75e5b85fec9ce9e84d2d6a5ff2c6f
linarahmatin <linarahmatin9@gmail.com>
2023-11-07 18:38:57 +0700

    hasil percobaan ke 2

--- joobsheet10/Numbers27.java
+++ joobsheet10/Numbers27.java
@@ -1,11 +1,14 @@
 package joobsheet10;
-import java.util.Scanner;
+import java.util.Arrays;
 public class Numbers27 {
     public static void main(String[] args) {
         int[][] myNumbers = new int[3][];
         myNumbers[0] = new int[5];
         myNumbers[1] = new int[3];
         myNumbers[2] = new int[1];
+
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println("Panjang baris ke-" + (i+1)+": "+myNumbers[i].length);
+        }
     }
-    
 }
