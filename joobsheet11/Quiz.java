1a2259bd6060caf7c9e0553f320048368885e235
linarahmatin <linarahmatin9@gmail.com>
2023-11-07 18:56:36 +0700

    hasil percobaan ke 4

--- joobsheet11/Quiz.java
+++ joobsheet11/Quiz.java
@@ -5,7 +5,6 @@ public class Quiz {
     public static void main(String[] args) {
         Random random = new Random();
         Scanner input = new Scanner(System.in);
-        
 
         char menu = 'y';
         do {
@@ -15,12 +14,17 @@ public class Quiz {
                 System.out.print("Tebak angka (1-10): ");
                 int answer = input.nextInt();
                 input.nextLine();
-                success = (answer == number);
+                if (answer < number) {
+                    System.out.println("Terlalu kecil. Coba lagi!");
+                } else if (answer > number) {
+                    System.out.println("Terlalu besar. Coba lagi!");
+                } else {
+                    System.out.println("Tebakan benar!");
+                    success = true;
+                }
             } while (!success);
             System.out.print("Apakah Anda ingin mengulang permainan (Y/y)? ");
             menu = input.nextLine().charAt(0);
         } while (menu == 'y' || menu == 'Y');
     }
 }
-    
-
